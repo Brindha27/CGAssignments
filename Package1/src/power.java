a6921a617b2a551b3ea97071b57e8e083714dbc2
Admin <Admin@LAPTOP-RSHIGOB3>
2021-04-07 21:34:14 +0530

    sdf

---
+++ Package1/src/power.java
@@ -0,0 +1,21 @@
+
+public class power {
+public static int checkPower(int n) {
+	if(n>0) {
+      while(n%2 == 0) {
+         n/=2;
+      }
+	}
+      return 0;
+	}
+      
+
+	public static void main(String[] args) {
+		int n=0;
+		
+		System.out.println(checkPower(n));
+		// TODO Auto-generated method stub
+
+	}
+
+}
